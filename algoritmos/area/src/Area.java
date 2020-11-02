8ca40cc5575b129fbd0d3a96a030375e2eeac6a5
Ana Paula da Silva Pereria <anapaulasilva1000@gmail.com>
2020-11-01 22:47:24 -0300

    Add files via upload

---
+++ algoritmos/area/src/Area.java
@@ -0,0 +1,49 @@
+public class Area {
+    public static void main(String[] args)
+    {
+        System.out.println("AreaCircle is " +  findAreaCircle(5));
+        System.out.println("AreaRectangule is " +  findAreaRectangule(5, 10));
+        System.out.println("AreaTriangle is " +  findAreaTriangle(5, 10));
+        System.out.println("AreaTrapezio is " +  findAreaTrapezio(10, 5, 6));
+        System.out.println("AreaCilindro is " +  findAreaCilindro(3, 6));
+        System.out.println("AreaCube is " +  findAreaCube(3));
+        System.out.println("AreaTetra is " +  findAreaTetra(3));
+    }
+
+    static final double PI = Math.PI;
+
+    static double findAreaCircle(int r)
+    {
+        return PI * Math.pow(r, 2);
+    }
+
+    static double findAreaRectangule(int b, int h)
+    {
+        return b * h;
+    }
+    static double findAreaTriangle(int b, int h)
+    {
+        return (b * h)/2;
+    }
+
+    static double findAreaTrapezio(int B, int b, int h)
+    {
+        return ((B+b) * h)/2;
+    }
+
+    static double findAreaCilindro(int r, int h)
+    {
+        return 2*PI*r*(r+h);
+    }
+
+    static double findAreaCube(int a)
+    {
+        return 6*(a*a);
+    }
+
+    static double findAreaTetra(int s)
+    {
+        double vol = (double)Math.round((Math.pow(s, 3) / (6 * Math.sqrt(2))) * 100) / 100;
+        return vol;
+    }
+}
